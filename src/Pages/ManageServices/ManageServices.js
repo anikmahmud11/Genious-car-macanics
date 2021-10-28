04dc08c3833be8d57250993aff0b3e7a5772da9f
Anik <baust.cse.160201078@gmail.com>
2021-10-28 19:56:12 +0600

    commit

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -4,12 +4,12 @@ import React, { useEffect, useState } from 'react';
 const ManageServices = () => {
     const [services, setServices] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/services')
+        fetch('https://intense-temple-04288.herokuapp.com/services')
             .then(res => res.json())
             .then(data => setServices(data))
     }, []);
     const handleDelete = id =>{
-        const url = `http://localhost:5000/services/${id}`;
+        const url = `https://intense-temple-04288.herokuapp.com/services/${id}`;
         fetch(url, {
             method:'DELETE'
         })
